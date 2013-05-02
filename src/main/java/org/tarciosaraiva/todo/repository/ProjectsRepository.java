49b9f0f6c7d646b702548f6c04fe2af0d5977658
Tarcio Saraiva <tarcio@tarcio.local>
2013-05-02 20:30:07 +1000

    Changes after presentation

--- src/main/java/org/tarciosaraiva/todo/repository/ProjectsRepository.java
+++ src/main/java/org/tarciosaraiva/todo/repository/ProjectsRepository.java
@@ -1,10 +1,18 @@
 package org.tarciosaraiva.todo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.tarciosaraiva.todo.domain.Project;
+
+import java.util.Date;
+import java.util.List;
 
 public interface ProjectsRepository extends JpaRepository<Project, Long> {
 
-//	List<Project> findByNameContaining(@Param("name") String name);
+	List<Project> findByNameContaining(@Param("name") String name);
+
+	@Query(name = "", value = "")
+	List<Project> findByDue(@Param("due") Date date);
 
 }
